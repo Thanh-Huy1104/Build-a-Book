a84ae90e2ea1b132742a2db3ec7197d3a9691eed
Hugo Vaillant <hugo.vaillant.1@ens.etsmtl.ca>
2024-01-20 16:39:23 -0500

    Book.jsx file

--- client/src/components/Book/Book.jsx
+++ client/src/components/Book/Book.jsx
@@ -1,5 +1,54 @@
+import { useState } from "react";
+import HTMLFlipBook from "react-pageflip"
+import { Page } from "./Page";
+
 export const Book = () => {
+
+  const pages = {
+    "images" : {
+      "1":"https://image.tmdb.org/t/p/original/53BC9F2tpZnsGno2cLhzvGprDYS.jpg",
+      "2":"https://image.tmdb.org/t/p/original/e9Qb2kmBnMXHCmNMI8NX1JbWhh1.jpg",
+      "3":"https://image.tmdb.org/t/p/original/2WjOOOGUu6dp4r8VqR5n48DY7JG.jpg",
+      "4":"https://image.tmdb.org/t/p/original/duIsyybgrC4S8kcCIVaxNOttV15.jpg",
+      "5":"https://image.tmdb.org/t/p/original/3RS8runn9AfrYDzRVPWuGPmvXQf.jpg",
+      "6":"https://image.tmdb.org/t/p/original/8wLRn2VvBlCu6cqYS4ypipnwosr.jpg",
+      "7":"https://image.tmdb.org/t/p/original/zDqVVkmfvj47FBUE5lwE4rWnITu.jpg",
+      "8":"https://image.tmdb.org/t/p/original/682Ui5DwZDdbIPzKAEOR7cJlMXa.jpg",
+      "9":"https://image.tmdb.org/t/p/original/6jdlppcnGi3XuJamfs4Vl7HyxB.jpg",
+      "10":"https://image.tmdb.org/t/p/original/uIq83ogs7QBEWi1aqmUrdDIH61m.jpg",
+    },
+
+    "sentences" : {
+      "1":"Fuck",
+      "2":"Shit",
+      "3":"Bitch",
+      "4":"Young",
+      "5":"Scheck",
+      "6":"Wes",
+      "7":"and",
+      "8":"I'm",
+      "9":"getting",
+      "10":"rich"
+    }
+  };
+
+  const keys = Object.keys(pages.images);
+
   return (
-    <div>Book</div>
-  )
+    <div className="flex items-center justify-center min-h-screen">
+    <HTMLFlipBook width={620} height={620}>
+      {keys.map((key, id) => (
+        <div className="items-center justify-center demoPage" key={id}>
+          <div className="absolute bottom-0 items-center justify-center mx-auto bg-white h-1/5 opacity-80">
+            <div className="flex items-center  w-[60%] justify-center h-full text-black">
+              <p>{pages.sentences[key]}</p>
+            </div>
+          </div>
+          <img src={pages.images[key]} alt={`Image ${key}`} className="w-full h-full" />
+        </div>
+        ))}
+      </HTMLFlipBook>
+    </div>
+);
+
 }
