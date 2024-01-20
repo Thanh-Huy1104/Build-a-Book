133bc5c26c698000c7052d4d0c2a54f64fe3721b
crashius <jasonrustom04@gmail.com>
2024-01-20 13:51:00 -0500

    Added footer - Added About-Background-Image

---
+++ client/src/components/Home/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className="Footer">
+        &copy; Than-Huy Nguyen, Anne-Marie Nguyen, Hugo 
+    </footer>
+  )
+}
+
+export default Footer
