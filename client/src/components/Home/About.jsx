ad76a0603c9663d5f4bbba57210aad05810cac08
crashius <jasonrustom04@gmail.com>
2024-01-20 12:22:54 -0500

    Added About.css, Imported Title Font

--- client/src/components/Home/About.jsx
+++ client/src/components/Home/About.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import './About.css'
 
 const About = () => {
   return (
-    <div>
-        <h1>About</h1>
-        <p></p>
+    <div className="About--div">
+        <h1 className="About--title">About</h1>
+        <p className="About--p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias facilis nulla quibusdam quam excepturi debitis dolorum quis, veritatis accusantium eaque doloremque, in voluptatem eos rerum aliquam quod magnam suscipit nisi?</p>
     </div>
   )
 }
-
 export default About;
