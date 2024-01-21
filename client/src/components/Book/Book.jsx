c6676a429d40416d5886fa45230caa1a98af7b03
Thanh-Huy1104 <etienne01.nguyen@gmail.com>
2024-01-21 02:21:13 -0500

    Update Book component styles and state variables

--- client/src/components/Book/Book.jsx
+++ client/src/components/Book/Book.jsx
@@ -9,7 +9,7 @@ import Animation from "../../assets/animation.json";
 import Loader from "../../assets/Loader.json";
 
 export const Book = () => {
-  const [test, setTest] = useState(null);
+  const [story, setStory] = useState(null);
   const [didRun, setDidRun] = useState(false);
   const [prompt, setPrompt] = useState(
     "Write about a story about a dog and a cat going on an adventure. They find a treasure, the treasure of friendship, only call them by cat or dog not any pronouns"
@@ -17,6 +17,7 @@ export const Book = () => {
 
   const [loopNumber, setLoopNumber] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
+  const [objKeys, setObjKeys] = useState([]);
   const toRotate = [
     "Bringing stories to life...",
     "Crafting magic...",
@@ -25,6 +26,34 @@ export const Book = () => {
   const [text, setText] = useState("");
   const period = 1000;
   const [delta, setDelta] = useState(50 - Math.random());
+
+  const pages = {
+    images: {
+      1: "https://image.tmdb.org/t/p/original/53BC9F2tpZnsGno2cLhzvGprDYS.jpg",
+      2: "https://image.tmdb.org/t/p/original/e9Qb2kmBnMXHCmNMI8NX1JbWhh1.jpg",
+      3: "https://image.tmdb.org/t/p/original/2WjOOOGUu6dp4r8VqR5n48DY7JG.jpg",
+      4: "https://image.tmdb.org/t/p/original/duIsyybgrC4S8kcCIVaxNOttV15.jpg",
+      5: "https://image.tmdb.org/t/p/original/3RS8runn9AfrYDzRVPWuGPmvXQf.jpg",
+      6: "https://image.tmdb.org/t/p/original/8wLRn2VvBlCu6cqYS4ypipnwosr.jpg",
+      7: "https://image.tmdb.org/t/p/original/zDqVVkmfvj47FBUE5lwE4rWnITu.jpg",
+      8: "https://image.tmdb.org/t/p/original/682Ui5DwZDdbIPzKAEOR7cJlMXa.jpg",
+      9: "https://image.tmdb.org/t/p/original/6jdlppcnGi3XuJamfs4Vl7HyxB.jpg",
+      10: "https://image.tmdb.org/t/p/original/uIq83ogs7QBEWi1aqmUrdDIH61m.jpg",
+    },
+
+    sentences: {
+      1: "Fuck",
+      2: "Shit",
+      3: "Bitch",
+      4: "Young",
+      5: "Scheck",
+      6: "Wes",
+      7: "and",
+      8: "I'm",
+      9: "getting",
+      10: "rich",
+    },
+  };
 
   useEffect(() => {
     let ticker = setInterval(() => {
@@ -76,9 +105,11 @@ export const Book = () => {
     const fetchStory = async () => {
       if (didRun) return;
       try {
-        const story = await getStory(prompt);
-        console.log("Received story:", story);
-        setTest(story);
+        const test = await getStory(input);
+        console.log("Received story:", test);
+        setStory(test);
+        setObjKeys(test.story.phrases);
+        console.log("test before setting:", test.story.phrases);
         console.log("test after setting:", test);
       } catch (error) {
         console.error("Error:", error);
@@ -89,24 +120,25 @@ export const Book = () => {
   }, []); // Include prompt in the dependency array to trigger a fetch when it changes
 
   return (
-    <div className="flex">
-      {test !== null ? (
+    <div className="">
+      {story !== null ? (
         <div>
           <Carousel>
-            {test.story.phrases.map((phrase, id) => {
+            {Object.keys(story.story.phrases).map((phrase, id) => {
+              console.log("hoe ");
               return (
                 <Paper key={id}>
                   <div className="big-container">
                     <div className="carousel-container">
                       <img
-                        src={`data:image/png;base64,${test.images[phrase][0].base64}`}
+                        src={`data:image/png;base64,${story.images[phrase][0].base64}`}
                         alt={"image"}
                         className="w-full h-full"
                       />
                     </div>
                     <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 w-[80%] h-[20%] rounded-xl">
                       <div className="flex items-center justify-center h-full p-6 text-black text-wrap">
-                        <p className="text ">{test.story.phrases[phrase]}.</p>
+                        <p className="text ">{story.story.phrases[phrase]}.</p>
                       </div>
                     </div>
                   </div>
